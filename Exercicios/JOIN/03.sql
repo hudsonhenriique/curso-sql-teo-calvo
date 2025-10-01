c7140cfcab99508cd9f4ea2c487bc19e688d8657
Hudson <hudson.henri10@gmail.com>
2025-10-01 15:57:02 -0300

    Exercicios de JOIN SQL

---
+++ Exercicios/JOIN/03.sql
@@ -0,0 +1,18 @@
+-- Do inicio ao fim do nosso curso (20255/08/25 a 2025/08/29), quantos clientes assinaram a lista de presença?
+
+SELECT count(DISTINCT t1.IdCliente) AS QtdClientes
+
+FROM transacoes AS t1
+
+LEFT JOIN transacao_produto AS t2
+ON t1.IdTransacao = t2.IdTransacao
+
+LEFT JOIN produtos AS t3
+ON t2.IdProduto = t3.IdProduto
+
+WHERE t1.DtCriacao >= '2025-08-25'
+AND t1.DtCriacao < '2025-08-30'
+AND t3.DescProduto = 'Lista de presença'
+
+
+
